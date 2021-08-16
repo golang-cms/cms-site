84bb55509af335aefaed22bb1cf352ca44006f0b
Tong Zhou <tong.zhou@indexexchange.com>
2021-08-16 10:38:11 -0400

    favicon

--- components/layout/onepirate/Layout.tsx
+++ components/layout/onepirate/Layout.tsx
@@ -1,15 +1,25 @@
-import styles from "../../../styles/Home.module.css";
-import Header from "./Header";
-import Footer from "./Footer";
+import { Box } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
+import React from "react";
+import styles from "../../../styles/Home.module.css";
+import Footer from "./Footer";
+import Header from "./Header";
+import Head from 'next/head';
 
 const Layout = ({ children, props }: any) => {
   return (
     <>
+      <Head>
+        <title>SmartDev Teams</title>
+        <meta name="description" content="Smart dev" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Header props={props} />
       <main className={styles.main}>{children}</main>
       <Footer props={props} />
-      <Button hidden={true} />
+      <Box display="none" displayPrint="block">
+        <Button hidden={true} />
+      </Box>
     </>
   );
 };
--- pages/_document.js
+++ pages/_document.js
@@ -18,6 +18,24 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/icon?family=Material+Icons"
             rel="stylesheet"
           />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/static/favicon/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/static/favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/static/favicon/site.webmanifest" />
         </Head>
         <body>
           <Main />
