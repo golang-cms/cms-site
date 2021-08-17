9357f8d3523cb24dc83037c731834cbaeddc5535
tong <zt1983811@gmail.com>
2021-08-17 01:06:19 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -37,7 +37,10 @@ module.exports = (phase) => {
       if (dev) {
         return defaultPathMap;
       }
-      return { "/": { page: "/placeholder" } };
+      return { 
+        "/": { page: "/placeholder" },
+        "/contact-us": { page: "/contact-us" },     
+      };
     },
     reactStrictMode: true,
     images: {
